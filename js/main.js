761b9cee56ab682f3bad30aa38ee9c9899956769
Alexander Rowland <arowland900@gmail.com>
2019-01-27 17:03:41 -0800

    add styling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /*----- constants -----*/
 var lookup = {
-    '1': 'purple',
-    '-1': 'lime',
-    'null': 'white'
+    '1': 'pink',
+    '-1': 'green',
+    'null': 'azure'
 };
 
 var winningCombos = [
@@ -33,15 +33,36 @@ document.querySelector('button').addEventListener('click', initialize);
 initialize();
 
 function handleMove(evt) {
-
+    // find index of square
+    var idx = parseInt(evt.target.id.replace('sq', ''))
+    // check to see if square is available, if not, return
+    if (board[idx] || winner) return;
+    // update state
+    board[idx] = turn
+    turn *= -1
+    winner = getWinner()
+    render()
 }
 
 function getWinner() {
-
+    for (var i = 0; i < winningCombos.length; i++) {
+        if (Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]) === 3) return board[winningCombos[i][0]];
+    }
+    if (board.includes(null)) return null;
+    return 'T';
 }
 
 function render() {
-
+    board.forEach(function (sq, idx) {
+        squares[idx].style.background = lookup[sq];
+    });
+    if (winner === 'T') {
+        message.innerHTML = 'Tie game!';
+    } else if (winner) {
+        message.innerHTML = `Congrats ${lookup[winner]}!`;
+    } else {
+        message.innerHTML = `${lookup[turn]}'s Turn`;
+    }
 }
 
 function initialize() {
